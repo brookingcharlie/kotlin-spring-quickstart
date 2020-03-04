32a23256c144cd46d43d426350d639746aa80e64
Charlie Brooking <charlie@brooking.id.au>
2020-03-04 14:20:55 +1000

    use SpringMockK, providing MockkBean similar to mockito MockBean

--- src/test/kotlin/org/example/ExampleControllerIT.kt
+++ src/test/kotlin/org/example/ExampleControllerIT.kt
@@ -1,16 +1,14 @@
 package org.example
 
+import com.ninjasquad.springmockk.MockkBean
 import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.junit5.MockKExtension
-import io.mockk.mockk
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.context.annotation.Bean
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
@@ -18,16 +16,10 @@ import org.springframework.test.web.servlet.get
 @WebMvcTest(ExampleController::class)
 @ExtendWith(MockKExtension::class)
 internal class ExampleControllerIT {
-    @TestConfiguration
-    class TestConfig {
-        @Bean
-        fun exampleService(): ExampleService = mockk(relaxed = true)
-    }
-
     @Autowired
     lateinit var mockMvc: MockMvc
 
-    @Autowired
+    @MockkBean(relaxed = true)
     lateinit var exampleService: ExampleService
 
     @BeforeEach
@@ -38,6 +30,7 @@ internal class ExampleControllerIT {
     @Test
     fun `should return message`() {
         every { exampleService.getMessage() } returns "Hello, world!"
+
         mockMvc.get("/example") {
             accept = MediaType.APPLICATION_JSON
         }.andExpect {
